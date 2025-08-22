d919c5191b2493ba206f1dd947dd66b578f3cf43
AfgunIsGun <109528450+AfgunIsGun@users.noreply.github.com>
2025-08-22 08:29:23 +0000

    fix(video): force fps to 25
    The video speed issue persists. As a test, this change hardcodes the FPS to 25 to see if it resolves the issue.

--- src/app/pages/translate/pose-viewers/pose-viewer.component.ts
+++ src/app/pages/translate/pose-viewers/pose-viewer.component.ts
@@ -73,8 +73,9 @@ export abstract class BasePoseViewerComponent extends BaseComponent implements O
   }
 
   async fps() {
-    const pose = await this.poseEl().nativeElement.getPose();
-    return pose.body.fps;
+    // const pose = await this.poseEl().nativeElement.getPose();
+    // return pose.body.fps;
+    return 25;
   }
 
   async initVideoEncoder(image: ImageBitmap) {
